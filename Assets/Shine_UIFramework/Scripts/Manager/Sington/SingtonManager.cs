72e21c336c51a9f12cca9307c36e8e8c562264aa
tianya <wly1990912@163.com>
2020-04-10 18:37:24 +0800

    [+]Init

---
+++ Assets/Shine_UIFramework/Scripts/Manager/Sington/SingtonManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Generic singleton Manager
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class SingtonManager<T> : MonoBehaviour where T : MonoBehaviour
+{
+    /// <summary>
+    /// Private constructor, avoid external instantiation
+    /// </summary>
+    private SingtonManager() { }
+
+    /// <summary>
+    /// Thread lock
+    /// </summary>
+    private static readonly object _lock = new object();
+}
